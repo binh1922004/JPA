c82fca2e455438ea3a8113890ef56db4cb00c663
binh1922004 <binh1920042018@gmail.com>
2024-10-08 22:16:29 +0700

    Video service (Interface + Impl)

---
+++ src/main/java/services/IVideoService.java
@@ -0,0 +1,23 @@
+package services;
+
+import entity.Video;
+
+import java.util.List;
+
+public interface IVideoService {
+        void insert(Video video);
+
+        void update(Video video);
+
+        void delete(int videoid) throws Exception;
+
+        Video findById(int videoid);
+
+        List<Video> findAll();
+
+        List<Video> findByVideoTilte(String title);
+
+        List<Video> findAll(int page, int pagesize);
+
+        int count();
+}
--- src/main/java/services/imp/VideoServiceImpl.java
+++ src/main/java/services/imp/VideoServiceImpl.java
@@ -1,5 +1,50 @@
 package services.imp;
 
-public class VideoServiceImpl {
+import dao.IVideoDao;
+import dao.imp.VideoDao;
+import entity.Video;
 
+import java.util.List;
+
+public class VideoServiceImpl implements services.IVideoService {
+        IVideoDao videoDao = new VideoDao();
+        @Override
+        public void insert(Video video){
+                videoDao.insert(video);
+        }
+
+        @Override
+        public void update(Video video){
+                videoDao.update(video);
+        }
+
+        @Override
+        public void delete(int videoid) throws Exception{
+                videoDao.delete(videoid);
+        }
+
+        @Override
+        public Video findById(int videoid){
+                return videoDao.findById(videoid);
+        }
+
+        @Override
+        public List<Video> findAll(){
+                return videoDao.findAll();
+        }
+
+        @Override
+        public List<Video> findByVideoTilte(String title){
+                return videoDao.findByVideoTilte(title);
+        }
+
+        @Override
+        public List<Video> findAll(int page, int pagesize){
+                return videoDao.findAll(page, pagesize);
+        }
+
+        @Override
+        public int count(){
+                return videoDao.count();
+        }
 }
